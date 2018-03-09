8444eb55eee13f8f7c25cec3b3cd01c3991f29dc
David Kesler <thedavidkesler@gmail.com>
2018-03-09 16:20:49 -0600

    button onClick pushes values to object of Employee class

--- client.js
+++ client.js
@@ -4,16 +4,33 @@ class Employee {
   constructor(firstNameIn, lastNameIn, idNumberIn, jobTitleIn, annualSalaryIn) {
     this.firstName = firstNameIn;
     this.lastName = lastNameIn;
-    this.idNumberIn = idNumber;
-    this.jobTitleIn = jobTitle;
-    this.annualSalaryIn = annualSalary;
+    this.idNumber = idNumberIn;
+    this.jobTitle = jobTitleIn;
+    this.annualSalary = annualSalaryIn;
   }
 }
+
+employeeArray = [];
 
 $(document).ready(readyNow);  //wait for doc to be ready before loading readyNow
 
 function readyNow () {
-console.log('in ready now');
-//firstName, lastName, idNumber, jobTitle, annualSalary
-$('#submitButton').on('click', submitButtonOnClick);
+
+  //firstName, lastName, idNumber, jobTitle, annualSalary
+  $('#submitButton').on('click', submitButtonOnClick);
   }//end readyNow
+
+
+function submitButtonOnClick() {
+    console.log('in submitButtonOnClick');
+    employeeEntered = new Employee;
+    employeeEntered.firstName = $('#firstNameInput').val();
+    employeeEntered.lastName = $('#lastNameInput').val();
+    employeeEntered.idNumber = $('#idNumberInput').val();
+    employeeEntered.jobTitle = $('#jobTitleInput').val();
+    employeeEntered.annualSalary = $('#annualSalaryInput').val();
+    employeeArray.push(employeeEntered);
+    
+
+
+}//end submitButtonOnClick
