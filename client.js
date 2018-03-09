e5501e886f3a6361f4e49878828d3ade88528d1a
David Kesler <thedavidkesler@gmail.com>
2018-03-09 16:57:03 -0600

    basic mode completed with table formatting tbd

--- client.js
+++ client.js
@@ -1,5 +1,7 @@
 console.log('js');
 
+let annualSalaryTotal = 0;
+
 class Employee {
   constructor(firstNameIn, lastNameIn, idNumberIn, jobTitleIn, annualSalaryIn) {
     this.firstName = firstNameIn;
@@ -25,12 +27,29 @@ function submitButtonOnClick() {
     console.log('in submitButtonOnClick');
     employeeEntered = new Employee;
     employeeEntered.firstName = $('#firstNameInput').val();
+    $('#firstNameInput').val('');
     employeeEntered.lastName = $('#lastNameInput').val();
+    $('#lastNameInput').val('');
     employeeEntered.idNumber = $('#idNumberInput').val();
+    $('#idNumberInput').val('');
     employeeEntered.jobTitle = $('#jobTitleInput').val();
-    employeeEntered.annualSalary = $('#annualSalaryInput').val();
+    $('#jobTitleInput').val('');
+    employeeEntered.annualSalary = parseInt($('#annualSalaryInput').val());
+    $('#annualSalaryInput').val('');
     employeeArray.push(employeeEntered);
-    
-
+    $('#employeeTable').append('<tr><td>' + employeeEntered.firstName +
+                               '</td><td>' + employeeEntered.lastName +
+                               '</td><td>' + employeeEntered.idNumber +
+                               '</td><td>' + employeeEntered.jobTitle +
+                               '</td><td>' + employeeEntered.annualSalary +
+                               '</td>');
+    annualSalaryTotal += employeeEntered.annualSalary;
+    $('#annualSalaryTotalDisplay').empty();
+    $('#annualSalaryTotalDisplay').append('Total Monthly: $' + annualSalaryTotal);
+    if ((annualSalaryTotal/12) > 20000) {
+      console.log('monthly total over 20000');
+      $('#annualSalaryTotalDisplay').css("background-color", "red");
+    }//end annualSalaryTotal if
+    console.log(annualSalaryTotal);
 
 }//end submitButtonOnClick
